232261353165199b578d7c19e080e45737158649
Asif Shakir <asif.shakir@gmail.com>
2024-03-09 18:35:47 +0530

    Missing Script and Certificate Files added

---
+++ Certificates/Generate-Certificates.ps1
@@ -0,0 +1,13 @@
+﻿$certificateName = "DemoBankCertificate"
+$certificatePassword = "FajrPassword@123"
+$certificateBaseFolder = "C:\Temp"
+$publicCertificatePath = Join-Path -Path $certificateBaseFolder -ChildPath "$($certificateName).cer"
+$privateCertificatePath = Join-Path -Path $certificateBaseFolder -ChildPath "$($certificateName).pfx"
+
+Write-Host $publicCertificatePath
+Write-Host $privateCertificatePath
+
+$cert = New-SelfSignedCertificate -DnsName $certificateName -CertStoreLocation "cert:\LocalMachine\My" -KeyExportPolicy Exportable -KeySpec Signature
+$pwd = ConvertTo-SecureString -String $certificatePassword -Force -AsPlainText
+Export-PfxCertificate -Cert $cert -FilePath $privateCertificatePath -Password $pwd
+Export-Certificate -Cert $cert -FilePath $publicCertificatePath
